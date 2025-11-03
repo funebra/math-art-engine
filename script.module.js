05279bbc27a2cb2fec250f9705d894576fb57fa1
Peter M. Lugha <106930077+funebra@users.noreply.github.com>
2025-11-03 17:02:28 +0100

    Update script.module.js

--- script.module.js
+++ script.module.js
@@ -254,6 +254,7 @@ export default {
         top: '140px',
         zIndex: 99999,
         whiteSpace: 'pre',
+        text-overflow: 'clip',
         fontFamily: 'ui-monospace, SFMono-Regular, Menlo, Consolas, monospace',
         fontSize: '12px',
         lineHeight: '1.35',
