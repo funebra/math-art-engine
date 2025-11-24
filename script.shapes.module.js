057cee6dc459c8fdf9554567acf2232e8ce4de04
Peter M. Lugha <106930077+funebra@users.noreply.github.com>
2025-11-24 10:06:08 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- script.shapes.module.js
+++ script.shapes.module.js
@@ -1328,6 +1328,8 @@ rotateX, rotateY,
   makeShape,
 };
 
+
+export { Funebra }
 export default Funebra;
 
 // Expose a browser global for legacy demos / MVX sCode
@@ -1339,6 +1341,7 @@ const polylineXh = "Funebra.polylineX(u, 24, 320,220, 380,180, 460,220, 520,300)
 const polylineYh = "Funebra.polylineY(u, 24, 320,220, 380,180, 460,220, 520,300)";
 const polylineClosedXh = "Funebra.polylineClosedX(u, 28, 360,160, 480,200, 460,300, 340,280)";
 const polylineClosedYh = "Funebra.polylineClosedY(u, 28, 360,160, 480,200, 460,300, 340,280)";
+
 
 
 
